34ed5acae0ec08de93aa81405f4e053294e97372
Uğur Ekinci <ugurekinci@Ugur-MacBook-Air.local>
2025-07-14 12:11:29 +0300

    vertexai_start.py file added.

---
+++ vertexai_start.py
@@ -0,0 +1,25 @@
+from google.auth.transport.requests import Request
+from google.oauth2.service_account import Credentials
+import vertexai
+
+api_key_path = "/Users/ugurekinci/Documents/Vertex_AI/vertex-ai-465718-b7ad113c10f1.json"
+
+credentials = Credentials.from_service_account_file(
+    api_key_path, scopes=["https://www.googleapis.com/auth/cloud-platform"]
+)
+
+PROJECT_ID = "vertex-ai-465718"
+REGION="us-central1"
+
+help(vertexai.init)
+
+# GCP ye ait olan Vertex AI platformunu kullanmak için Python ortamında bir başlangıç -
+# noktası oluşturur.
+# Bu metod, Vertex AI hizmetleriyle çalışmaya başlamadan önce gerekli kimlik doğrulama-
+# ve yapılandırma ayarlarını yapar.
+vertexai.init(
+    project = PROJECT_ID,
+    location = REGION,
+    credentials = credentials
+)
+
